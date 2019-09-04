8ca76e075dc347b71af3602665bcbd0fd3d287ff
Kamil Zawilski <zawilskikamil@gmail.com>
2019-09-04 19:28:12 +0200

    add map list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import MapList from './app/components/mapList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 2
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    mapPoints: []
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:8080/map')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ mapPoints: data })
+        })
+        .catch(console.log)
+  }
+
+  render() {
+    return (
+        <MapList mapList={this.state.mapPoints} />
+    );
+  }
 }
 
 export default App;
